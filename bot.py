2c5ba4c47d0fbd10f280ad63a39bf0de3934bac3
Jasper Seabold <jasperseabold@Jaspers-MacBook-Air.local>
2021-07-13 22:57:14 -0400

    modified bot.py to use discord.ext.commands.Bot class and implemented 'quote' and 'dd' commands using yfinance

--- bot.py
+++ bot.py
@@ -1,15 +1,33 @@
 import os
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+import yfinance as yf
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="$")
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+@bot.command(name="quote", help="Fetches stock quote for provided ticker")
+async def quote(ctx, symbol):
+    print("received")
+    CATEGORIES = ("shortName", "currentPrice", "open", "previousClose", "volume24Hr")
+    LABELS = {"shortName": "Name", 
+        "currentPrice": "Current Price", 
+        "open": "Market Open", 
+        "previousClose": "Previous Close", 
+        "volume24Hr": "24 Hour Volume"
+    }
+    ticker = yf.Ticker(symbol)
+    response = "```" + "\n".join([f"{LABELS[category]}: {ticker.info[category]}" for category in CATEGORIES]) + "```"
+    await ctx.send(response)
 
-client.run(TOKEN)
+@bot.command(name="dd", help="Fetches information on provided ticker")
+async def dd(ctx, symbol):
+    print("received")
+    ticker = yf.Ticker(symbol)
+    response = "```" + "\n".join([f"{key}: {ticker.info[key]}" for key in ticker.info.keys()])[:1994] + "```"
+    await ctx.send(response)
+
+bot.run(TOKEN)
