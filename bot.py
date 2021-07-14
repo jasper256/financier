1f6fb98ad3f9a6b8b540e59783126be95a123b10
Jasper Seabold <jasperseabold@Jaspers-MacBook-Air.local>
2021-07-14 10:47:35 -0400

    implemented  command using GoldAPI

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
+import requests
 import textwrap
 import yfinance as yf
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GoldAPI_KEY = os.getenv("GoldAPI_KEY")
 
 bot = commands.Bot(command_prefix="$")
 
@@ -31,5 +33,39 @@ async def dd(ctx, symbol):
     response = "\n".join([f"{key}: {ticker.info[key]}" for key in ticker.info.keys()])
     for line in textwrap.wrap(response, 1996):
         await ctx.send(f"```{line}```")
+
+@bot.command(name="spot", help="Returns spot price of provided precious metal")
+async def spot(ctx, symbol):
+    print("recieved")
+    METALS = {
+        "gold": "XAU",
+        "au": "XAU",
+        "silver": "XAG",
+        "ag": "XAG",
+        "platinum": "XPT",
+        "pt": "XPT",
+        "palladium": "XPD",
+        "pd": "XPD"
+    }
+    if symbol.lower() not in METALS:
+        await ctx.send("`Invalid selection`")
+        return
+    url = f"https://www.goldapi.io/api/{METALS[symbol.lower()]}/USD/"
+    headers = {
+        "x-access-token": GoldAPI_KEY,
+        "Content-Type": "application/json"
+    }
+    payload = requests.get(url, headers=headers).json()
+    CATEGORIES = ("symbol", "prev_close_price", "open_price", "price")
+    LABELS = {
+        "symbol": "Symbol",
+        "prev_close_price": "Previous Close",
+        "open_price": "Open",
+        "price": "Price"
+    }
+    response = "\n".join([f"{LABELS[category]}: {payload[category]}" for category in CATEGORIES])
+    await ctx.send(f"```{response}```")
+
+
 
 bot.run(TOKEN)
